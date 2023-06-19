c287c8be0f83b0357dce51f7bb8f50f093ae8300
Felipe Telles <felipe.melo.telles@gmail.com>
2023-06-18 22:28:40 -0300

    showing voltage and current

--- Src/application/electrical_analyzer.c
+++ Src/application/electrical_analyzer.c
@@ -2,8 +2,8 @@
 
 #include "stm32f1xx_hal.h"
 
-#define NUM_CHANNELS 4
-#define GAIN_ADC     (4.095 / 3.3)
+#define NUM_CHANNELS     4
+#define ADC_BIT_TO_mV(x) (((3300000 / 4095) * x) / 1000)
 
 #define ADC_CHANNEL_LUX         0
 #define ADC_CHANNEL_TEMPERATURE 1
@@ -14,6 +14,24 @@
 #define TEMPERATURE_INTERCEPT -25.505305176557748f
 #define LUX_SLOPE             0.911390660003446f
 #define LUX_INTERCEPT         -425.5767706358779f
+
+#define SQUARE_ROOT_x1000 1414
+
+#define VOLTAGE_REDUCED_MAX_mV    3160
+#define VOLTAGE_REDUCED_MIN_mV    200
+#define VOLTAGE_REAL_MAX_x1000    (127 * SQUARE_ROOT_x1000)
+#define VOLTAGE_REDUCED_OFFSET_mV ((VOLTAGE_REDUCED_MIN_mV + VOLTAGE_REDUCED_MAX_mV) / 2)
+#define VOLTAGE_REAL_GAIN_x1000                                                          \
+    (VOLTAGE_REAL_MAX_x1000 / (VOLTAGE_REDUCED_MAX_mV - VOLTAGE_REDUCED_OFFSET_mV))
+#define VOLTAGE_REDUCED_TO_REAL(x) ((VOLTAGE_REAL_GAIN_x1000 * x) / 1000)
+
+#define CURRENT_REDUCED_MAX_mV    2760
+#define CURRENT_REDUCED_MIN_mV    80
+#define CURRENT_REAL_MAX_x1000    (200 * 1000)
+#define CURRENT_REDUCED_OFFSET_mV ((CURRENT_REDUCED_MIN_mV + CURRENT_REDUCED_MAX_mV) / 2)
+#define CURRENT_REAL_GAIN_x1000                                                          \
+    (CURRENT_REAL_MAX_x1000 / (CURRENT_REDUCED_MAX_mV - CURRENT_REDUCED_OFFSET_mV))
+#define CURRENT_REDUCED_TO_REAL(x) ((CURRENT_REAL_GAIN_x1000 * x) / 1000)
 
 extern ADC_HandleTypeDef hadc1;
 
@@ -27,11 +45,17 @@ void init_electrical_analyzer(void) {
 void adc_conversion_complete_callback(void) {}
 
 int16_t get_voltage(void) {
-    return adc_buf[ADC_CHANNEL_VOLTAGE];
+    const int32_t voltage_mV =
+        ADC_BIT_TO_mV(adc_buf[ADC_CHANNEL_VOLTAGE]) - VOLTAGE_REDUCED_OFFSET_mV;
+    const int16_t voltage_V = (int16_t)VOLTAGE_REDUCED_TO_REAL(voltage_mV);
+    return voltage_V;
 }
 
 int16_t get_current(void) {
-    return adc_buf[ADC_CHANNEL_CURRENT];
+    const int32_t current_mV =
+        ADC_BIT_TO_mV(adc_buf[ADC_CHANNEL_CURRENT]) - CURRENT_REDUCED_OFFSET_mV;
+    const int16_t current_V = (int16_t)CURRENT_REDUCED_TO_REAL(current_mV);
+    return current_V;
 }
 
 float get_lux(void) {
@@ -48,5 +72,5 @@ float get_temperature(void) {
 }
 
 int16_t get_power(void) {
-    return 0;
+    return get_voltage() * get_current();
 }
